fd1b91f9457b3e79319724fdde5817f633ecaaa2
Nilsiker <nilsiker94@gmail.com>
2020-05-17 18:31:13 +0200

    reformat, tidy as heck now

--- edaf25/edaf25-labs/src/main/java/lab5/Lab5.java
+++ edaf25/edaf25-labs/src/main/java/lab5/Lab5.java
@@ -15,80 +15,59 @@ public class Lab5 {
      * @return shortest distance between start and end
      */
     public static int maxFlow(FlowGraph g, int source, int sink) {
-        int count = g.vertexCount();            // no of vertices
-        int paths = 0;
-        // Graph variables
-        int[][] flow = new int[count][count];   // flow[x][y] returns flow in edge x-to-y
-        int[][] cap = new int[count][count];    // cap[x][y] returns remaining capacity in edge x-to-y
-        for (int j = 0; j < count; j++){
-            for (int i= 0; i<count; i++){
-                cap[i][j] = g.getCapacity(i,j);
+        int numVertex = g.vertexCount();                            // no of vertices in graph
+        int flow = 0;                                               // the max flow in the graph
+        int[] pred = new int[numVertex];                            // holds predecessor nodes in path
+        int[][] residual = new int[numVertex][numVertex];           // residual graph
+
+
+        for (int j = 0; j < numVertex; j++)                         //
+            for (int i = 0; i < numVertex; i++)                     // Set residual graph capacities to original graph
+                residual[i][j] = g.getCapacity(i, j);               //
+
+        while (bfs(numVertex, source, sink, residual, pred)) {              // As long as we keep finding augment paths
+            int bottleneck = residual[pred[sink]][sink];                        // Find the bottleneck in the path
+            for (int i = sink; i != source; i = pred[i])                        // iterate over path
+                bottleneck = Math.min(bottleneck, residual[pred[i]][i]);        // set bottleneck to smallest residual capacity
+
+            for (int i = sink; i != source; i = pred[i]) {                  // Update residual capacities
+                residual[pred[i]][i] -= bottleneck;                         // Forward edges get subtracted
+                residual[i][pred[i]] += bottleneck;                         // Backwards edges get added
             }
+            flow += bottleneck;                                             // add the bottleneck capacity in this path to max flow
         }
 
-        while(true){    // dirty...
-            paths++;
-            // Queue variables
-            int current;                                // current node
-            boolean[] visited = new boolean[count];     // visited "set"
-            int[] previous = new int[count];            // previous[x] returns the previous node to x, in the path
-            boolean atSink = false;
-            Queue<Integer> q = new PriorityQueue<>();   // queue for BFS
-            q.add(source);                              // add the first node to queue
+        return flow;                                                // When no more augment paths can be found, max flow is found!
+    }
 
-            Arrays.fill(visited,false);             // defaults all visited to false
-            visited[source] = true;                     // but source is always visited
+    private static boolean bfs(int numVertex, int source, int sink, int[][] residual, int[] pred) {
+        int current;                                        // the current node
+        boolean[] visited = new boolean[numVertex];         // holds if a node is visited already or not
+        Arrays.fill(visited, false);                    // default all nodes as NOT VISITED
+        visited[source] = true;                             // but start node is always visited
 
-            // Find shortest path
-            while(!q.isEmpty()){
-                if(q.peek() == sink){
-                    atSink = true;
-                    break;
+        Queue<Integer> q = new PriorityQueue<>();           // the queue
+        q.add(source);                                      // add start node to queue to get started!
+
+        while (!q.isEmpty()) {
+            if (q.peek() == sink)                                   // we've arrived at the sink, a path is found!
+                return true;
+            current = q.poll();                                     // get the next node
+            for (int i = 0; i < numVertex; i++) {
+                if (!visited[i] && residual[current][i] > 0) {      // if other edge to node has POSITIVE RESIDUAL CAPACITY
+                    visited[i] = true;                              // mark node as visited
+                    q.add(i);                                       // add node to queue
+                    pred[i] = current;                              // set the node's predecessor to the current node
                 }
-
-                current = q.poll();
-                for (int i = 0; i < count; i++) {
-                    if(!visited[i] && cap[current][i] > flow[current][i]){
-                        visited[i] = true;
-                        q.add(i);
-                        previous[i] = current;
-                    }
-                }
-            }
-            if(!atSink) break;  // if the queue is empty and we haven't reached the sink, we have augmented all paths!
-
-            // Finds the bottleneck in the path
-            int bottleneck = cap[previous[sink]][sink] - flow[previous[sink]][sink];
-            for (int i = sink; i != source; i = previous[i]){
-                bottleneck = Math.min(bottleneck, (cap[previous[i]][i] - flow[previous[i]][i]));
-            }
-
-            // Augment path
-            for (int i = sink; i != source; i = previous[i]) {
-                flow[previous[i]][i] += bottleneck;
-                flow[i][previous[i]] -= bottleneck;
             }
         }
-
-        // Calculate flow
-        int maxFlow = 0;
-        for (int i = 0; i < count; i++) {
-            maxFlow += flow[source][i];
-        }
-
-        return maxFlow;
+        return false;                                       // the queue is empty and we didn't arrive at the sink :(
     }
 
     /**
      * Read a flowgraph from a file.
      */
     public static FlowGraph loadFlowGraph(Path path) throws IOException {
-        // TODO(D3): läs in ett flödesnätverk från fil.
-        // Filen börjar med ett heltal som anger antalet noder,
-        // sedan följer ett tal m som är antalet bågar.
-        // Resten av filen består av m rader där varje rad anger en båge i
-        // formatet u v c som beskriver en båge från en nod u till v med kapacitet c.
-
         int vertexCount;
         int edgeCount;
         FlowEdge[] edges;
@@ -107,7 +86,7 @@ public class Lab5 {
             int src = Integer.parseInt(edgeParts[0]);
             int dest = Integer.parseInt(edgeParts[1]);
             int cap = Integer.parseInt(edgeParts[2]);
-            if(cap<0) cap = Integer.MAX_VALUE;
+            if (cap < 0) cap = Integer.MAX_VALUE;
             edges[i] = new FlowEdge(src, dest, cap);
         }
 
